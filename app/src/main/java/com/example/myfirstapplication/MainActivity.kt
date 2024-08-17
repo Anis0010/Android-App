8292febf77c299bac85a8a0fc65cd2d4084844e8
Anis0010 <youssefanis48@gmail.com>
2024-08-17 17:07:16 +0300

    login_screen

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.myfirstapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,13 +14,28 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.data.Group
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.databinding.DataBindingUtil
+import com.example.myfirstapplication.R.string
+import com.example.myfirstapplication.databinding.ConstraintBinding
 import com.example.myfirstapplication.ui.theme.MyFirstApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
-    }
-}
+       setContentView(R.layout.login_screen)
+
+
+
+            }
+        }
+
+
+
+
+
